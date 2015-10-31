0bbb39235214186c45ad7ead160f25e230de0e38
Mariella Pucher <mariella.pucher@gmx.at>
2015-10-31 08:35:31 +0100

    LV-Termin1

---
+++ src/Demo.java
@@ -0,0 +1,20 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean treeFront = true;
+		int numberLeaves = 0;
+		char meinZeichen ='X';
+		
+		System.out.println("Ist Baum voraus? "+treeFront);
+		System.out.println();
+		System.out.println("Anzahl der Blätter: "+numberLeaves);
+		numberLeaves = numberLeaves + 1;
+		System.out.println("Anzahl der Blätter: "+numberLeaves);
+		System.out.println(meinZeichen);
+		
+	}
+
+}
---
+++ src/Demo2.java
@@ -0,0 +1,18 @@
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 9;
+		int b = 5;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		System.out.println(a + " + " + b + " = " + (a+b));
+		System.out.println(a + " - " + b + " = " + (a-b));
+		System.out.println(a + " * " + b + " = " + (a*b));
+		System.out.println(a + " / " + b + " = " + (a/b));
+		System.out.println(a + " % " + b + " = " + (a%b));
+	}
+
+}
